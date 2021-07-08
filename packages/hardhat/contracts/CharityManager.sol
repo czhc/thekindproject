f8b21bc7f2d43e51ae8784f533585f24ae7c07ca
czhc <cedriczhchan@gmail.com>
2021-07-08 10:31:58 +0800

    Setup CharityManager and Charity classes. - Charity contracts are payable, managed by beneficiary wallets. - Manager is public/donor interface

--- packages/hardhat/contracts/CharityManager.sol
+++ packages/hardhat/contracts/CharityManager.sol
@@ -45,37 +45,49 @@ contract Charity {
         uint256 ammount;
     }
 
-    address payable public owner;
+    CharityManager public manager;
+    address private owner;
     string public name;
     uint public index;
-    CharityManager manager;
     Status public status;        
 
     uint256 public balance;
 
-    event fundWithdrawn(address _to, uint256 _amount);
-    event fundReceived(address _from, uint256 _amount);
+    event FundWithdrawn(address _to, uint256 _amount);
+    event FundReceived(address _from, uint256 _amount);
     
     constructor(CharityManager _manager, address _wallet, string memory _name, uint _index) {
         manager = _manager;
-        owner = payable(_wallet);
+        owner = _wallet;
         name = _name;
         index = _index;
         status = Status.New;
     }
 
-    function verify() public {
-        require(msg.sender == manager.owner(), "Unauthorized");
-        status = Status.Verified;
-        manager.verifyCharity(index);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Unauthorized owner");
+        _;
+    }
+    modifier onlyManager() {
+        /*
+            All non-payable calls should come from Manager contract
+        */
+        require(msg.sender == address(manager), "Unauthorized manager");
+        _;
     }
 
+    function getOwner() public view returns (address) {
+        return owner;
+    }
 
-    function withdraw(uint256 amount) public payable {
-        require(msg.sender == owner, "Unauthorized");
+    function verify() public onlyManager {
+        status = Status.Verified;
+    }
+
+    function withdraw(uint256 amount) public payable onlyOwner {
         require(balance >= amount, "Insufficient balance");
-        owner.transfer(amount);
-        emit fundWithdrawn(owner, msg.value);
+        payable(owner).transfer(amount);
+        emit FundWithdrawn(owner, msg.value);
     }
 
     function withdrawAll() public payable {
@@ -85,11 +97,10 @@ contract Charity {
     receive() external payable {
         require(status == Status.Verified, "Charity is not Verified");
         balance += msg.value;
-        emit fundReceived(msg.sender, msg.value);
+        emit FundReceived(msg.sender, msg.value);
     }
 
-    fallback() external {
-        
+    fallback() external payable{
     }
 
 }
